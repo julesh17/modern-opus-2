5aea019d26e4df80e113e817f58f95520a825445
julesh17 <107618459+julesh17@users.noreply.github.com>
2025-11-23 01:06:22 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <--- VÉRIFIE CETTE LIGNE À LA LETTRE PRÈS
   ],
   theme: {
     extend: {
